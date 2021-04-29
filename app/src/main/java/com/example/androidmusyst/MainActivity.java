74d59f9a2bbb15498ac6130cfb20748dbbb751fa
Tom <tom-vautray@hotmail.fr>
2021-04-29 14:59:27 +0200

    Intent bound PlaylistActivity to MainActivity

--- app/src/main/java/com/example/androidmusyst/MainActivity.java
+++ app/src/main/java/com/example/androidmusyst/MainActivity.java
@@ -39,5 +39,15 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
             }
         });
+
+        playlistButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, PlaylistActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
