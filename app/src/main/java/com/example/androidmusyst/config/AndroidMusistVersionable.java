cd37092525545de7e41d4663f7afe04b66344e55
Tom <tom-vautray@hotmail.fr>
2021-04-29 20:36:55 +0200

    Versions setting

--- app/src/main/java/com/example/androidmusyst/config/AndroidMusistVersionable.java
+++ app/src/main/java/com/example/androidmusyst/config/AndroidMusistVersionable.java
@@ -2,17 +2,23 @@ package com.example.androidmusyst.config;
 
 import com.example.androidmusyst.Versionyst.src.versionystPackage.*;
 
-public class AndroidMusistVersionable extends versionystPackage.Versionable
+public class AndroidMusistVersionable extends Versionable
 {
     public AndroidMusistVersionable()
     {
         super();
-        getDependencies().put("Versionyst", 4);
+        getDependencies().put("Versionyst", 5);
         getDependencies().put("ResourcystVersionable", 5);
+        getDependencies().put("MusicFindystVersionable", 9);
     }
 
     @Override
     public Integer getVersionId() {
-        return null;
+        return 1;
+    }
+
+    @Override
+    protected String getPackageName() {
+        return "AndroidMusyst";
     }
 }
