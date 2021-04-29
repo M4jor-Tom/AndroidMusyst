d74c8d2fa99f00ca2c778fbf8ed731705e9d5535
Tom <tom-vautray@hotmail.fr>
2021-04-29 14:57:15 +0200

    Added playlist activity

---
+++ app/src/main/java/com/example/androidmusyst/PlaylistActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidmusyst;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlaylistActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playlist);
+    }
+}
