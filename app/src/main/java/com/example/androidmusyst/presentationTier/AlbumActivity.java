0ca94f9acb6c41b41e41be505250377ff21dbadf
Tom <tom-vautray@hotmail.fr>
2021-05-02 22:28:53 +0200

    Added SearchBar and MaterialButton to Album page

--- app/src/main/java/com/example/androidmusyst/presentationTier/AlbumActivity.java
+++ app/src/main/java/com/example/androidmusyst/presentationTier/AlbumActivity.java
@@ -1,17 +1,50 @@
 package com.example.androidmusyst.presentationTier;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import com.example.androidmusyst.R;
+import com.google.android.material.button.MaterialButton;
 
 public class AlbumActivity extends AppCompatActivity
 {
+    private static EditText _searchBar;
+    private static MaterialButton _searchMaterialButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
+
+        setSearchBar((EditText)findViewById(R.id.albumSearchBar));
+        setSearchMaterialButton((MaterialButton)findViewById(R.id.albumSearchMaterialButton));
+    }
+
+    public static EditText getSearchBar()
+    {
+        return _searchBar;
+    }
+
+    protected static void setSearchBar(EditText searchBar)
+    {
+        _searchBar = searchBar;
+    }
+
+    protected static MaterialButton getSearchMaterialButton()
+    {
+        return _searchMaterialButton;
+    }
+
+    protected static void setSearchMaterialButton(MaterialButton searchMaterialButton)
+    {
+        _searchMaterialButton = searchMaterialButton;
     }
 }
