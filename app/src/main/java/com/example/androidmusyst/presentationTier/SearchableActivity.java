3c5cf1bef2ce3d49beaf2874033a21cc340fd89a
Tom <tom-vautray@hotmail.fr>
2021-05-03 23:11:03 +0200

    Added back SearchableActivity extends AppCompatActivity

---
+++ app/src/main/java/com/example/androidmusyst/presentationTier/SearchableActivity.java
@@ -0,0 +1,51 @@
+package com.example.androidmusyst.presentationTier;
+
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
+import com.google.android.material.button.MaterialButton;
+
+abstract public class SearchableActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String>
+{
+    private EditText _searchBar;
+    private MaterialButton _searchMaterialButton;
+
+
+
+    public EditText getSearchBar()
+    {
+        return _searchBar;
+    }
+
+    protected void setSearchBar(EditText searchBar)
+    {
+        _searchBar = searchBar;
+    }
+
+    protected MaterialButton getSearchMaterialButton()
+    {
+        return _searchMaterialButton;
+    }
+
+    protected void setSearchMaterialButton(MaterialButton searchMaterialButton)
+    {
+        _searchMaterialButton = searchMaterialButton;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data)
+    {
+        System.out.println(toString() + "::onLoadFinished() " + data);
+        getSearchBar().setText(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader)
+    {
+
+    }
+}
